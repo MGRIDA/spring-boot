57ac303a92c27aca0dd28d5ecc652f96fe08cdf8
MGRIDA <mgrida28@gmail.com>
2017-12-15 21:44:29 +0100

    init master branch

---
+++ src/main/java/com/mg/rida/formationspring/FormationSpringApplication.java
@@ -0,0 +1,12 @@
+package com.mg.rida.formationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mg/rida/formationspring/FormationSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mg.rida.formationspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormationSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
